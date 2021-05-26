a92b2e5257743eca9afe0314dbe5cf8f4d73d5be
Ville Kopio <ville.kopio@kodan.fi>
2021-05-26 23:45:34 +0300

    Fix a data import bug as there are no headers.

--- src/common.py
+++ src/common.py
@@ -3,13 +3,10 @@ import pandas as pd
 
 from sklearn.model_selection import cross_val_score
 
-train_true = pd.read_csv('../data/train_true_bow.csv')
-train_fake = pd.read_csv('../data/train_fake_bow.csv')
+train_true = pd.read_csv('../data/train_true_bow.csv', header=None)
+train_fake = pd.read_csv('../data/train_fake_bow.csv', header=None)
 
-train_data = pd.concat(
-  [train_true, train_fake]
-).fillna(0).astype('int64').to_numpy()
-
+train_data = pd.concat([train_true, train_fake]).to_numpy()
 train_target = np.array([1] * len(train_true) + [0] * len(train_fake))
 
 def cv(model, data, target):
