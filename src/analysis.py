69c9f5c8a3bc7325154e9c23262ebfb1b336cbda
Ville Kopio <ville.kopio@kodan.fi>
2021-05-27 11:40:19 +0300

    Add pca variance explained plot.

--- src/analysis.py
+++ src/analysis.py
@@ -17,5 +17,13 @@ principalDf = pd.DataFrame(
 
 principalDf['target'] = train_target
 
-sns.scatterplot(data=principalDf, x='pc1', y='pc2', hue='target')
+sns.scatterplot(data=principalDf, x='pc1', y='pc2', hue='target', alpha=0.3)
+plt.show()
+
+pca_full = PCA()
+pca_full.fit_transform(train_data)
+
+plt.plot(np.cumsum(pca_full.explained_variance_ratio_))
+plt.xlabel('number of components')
+plt.ylabel('cumulative explained variance')
 plt.show()
