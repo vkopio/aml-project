b16ad2299c7c032ec1ce262635a30e395c2a326c
Ville Kopio <ville.kopio@kodan.fi>
2021-05-30 17:23:56 +0300

    Gradient boost parameter optimization.

--- src/model_gboost.py
+++ src/model_gboost.py
@@ -17,14 +17,91 @@ from common import train_data, train_target, cv, make_test_prediction
 # plt.legend()
 # plt.show()
 
-gboost = GradientBoostingClassifier(
-  n_estimators=2000, 
-  learning_rate=1.0,
-  max_depth=1, 
-  random_state=42)
+# for n in range(3, 4):
+#     gboost = GradientBoostingClassifier(
+#         n_estimators=1000,
+#         learning_rate=0.1,
+#         max_depth=n,
+#         random_state=42)
 
-cv_score = cv(gboost, train_data, train_target)
+#     cv_score = cv(gboost, train_data, train_target)
 
 #gboost.fit(train_data, train_target)
 
 #make_test_prediction(gboost, 'gradientboost')
+
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score, GridSearchCV
+
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 12, 4
+
+
+def modelfit(alg, performCV=True, printFeatureImportance=True, cv_folds=3):
+    # Fit the algorithm on the data
+    alg.fit(train_data, train_target)
+
+    # Predict training set:
+    dtrain_predictions = alg.predict(train_data)
+    dtrain_predprob = alg.predict_proba(train_data)[:, 1]
+
+    # Perform cross-validation:
+    if performCV:
+        cv_score = cross_val_score(
+            alg, train_data, train_target, cv=cv_folds)
+
+    # Print model report:
+    print("\nModel Report")
+    print("Accuracy : %.4g" % metrics.accuracy_score(
+        train_target, dtrain_predictions))
+    print("AUC Score (Train): %f" %
+          metrics.roc_auc_score(train_target, dtrain_predprob))
+
+    if performCV:
+        print("CV Score : Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g" %
+              (np.mean(cv_score), np.std(cv_score), np.min(cv_score), np.max(cv_score)))
+
+    # Print Feature Importance:
+    if printFeatureImportance:
+        feat_imp = pd.Series(
+            alg.feature_importances_).sort_values(ascending=False)
+        feat_imp.plot(kind='bar', title='Feature Importances')
+        plt.ylabel('Feature Importance Score')
+        plt.show()
+
+
+gbm = GradientBoostingClassifier(
+    learning_rate=0.02,
+    n_estimators=700,
+    min_samples_split=1400,
+    min_samples_leaf=30,
+    max_depth=11,
+    max_features=3,
+    subsample=0.8,
+    random_state=42
+)
+#modelfit(gbm)
+
+#gbm.fit(train_data, train_target)
+#make_test_prediction(gbm, 'gradientboost_v2')
+
+param_test1 = {'n_estimators': range(700,3000,300), 'learning_rate': [0.01, 0.02]}
+
+gsearch1 = GridSearchCV(
+    estimator=gbm,
+    param_grid=param_test1,
+    scoring='accuracy',
+    n_jobs=4,
+    cv=5
+)
+
+gsearch1.fit(train_data, train_target)
+
+print(gsearch1.cv_results_)
+print(gsearch1.best_params_)
+print(gsearch1.best_score_)
+
+#cv_score = cv(gbm, train_data, train_target)
